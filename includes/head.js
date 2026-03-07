b2d0dd34e152a3b7ac180422f2e9a4628f212af3
cantinbruno <133025636+cantinbruno@users.noreply.github.com>
2026-03-07 01:01:48 +0000

    Update head.js

--- includes/head.js
+++ includes/head.js
@@ -1,21 +1,13 @@
 class SiteHead extends HTMLElement {
   connectedCallback() {
-
-    const title = this.getAttribute("title") || "Audit Radar";
-    const description = this.getAttribute("description") 
-      || "Audit Radar : Auditer votre système depuis internet en un clique.";
-
     document.head.innerHTML = `
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <title>${title}</title>
-
-      <meta name="description" content="${description}">
+      <title>Audit Radar</title>
+      <meta name="description" content="Audit Radar : Auditer votre système depuis internet en un clic.">
       <meta name="robots" content="index, follow">
       <meta http-equiv="content-language" content="fr">
       <meta name="author" content="Audit Radar">
-      
     `;
   }
 }
