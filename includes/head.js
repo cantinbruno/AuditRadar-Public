7ee9d92348c84b135d30b039f1147f38a954bee6
cantinbruno <133025636+cantinbruno@users.noreply.github.com>
2026-03-07 23:46:56 +0000

    Update head.js

--- includes/head.js
+++ includes/head.js
@@ -1,8 +1,6 @@
 class SiteHead extends HTMLElement {
   connectedCallback() {
     document.head.innerHTML = `
-      <link rel="icon" href="/img/favicon.ico" sizes="any">
-      <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Audit Radar</title>
