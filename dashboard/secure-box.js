0bd1b6d54d20eec7100dae83a9252fe88506af1e
cantinbruno <133025636+cantinbruno@users.noreply.github.com>
2026-03-09 00:20:23 +0000

    JH

--- dashboard/secure-box.js
+++ dashboard/secure-box.js
@@ -4,7 +4,6 @@ class SecureBox extends HTMLElement {
       <section id="protectedContent">
         
         <navigation-bar></navigation-bar>
-        <script src="/dashboard/navigation.js"></script>
 
         <div class="ar-secure__header">
           <h2 class="ar-secure__title">Espace sécurisé</h2>
