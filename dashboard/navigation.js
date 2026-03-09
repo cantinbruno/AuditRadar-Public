2f2a0fbe935583c76aaaad413a9490811664a1fb
cantinbruno <133025636+cantinbruno@users.noreply.github.com>
2026-03-09 02:04:48 +0000

    oui

--- dashboard/navigation.js
+++ dashboard/navigation.js
@@ -9,40 +9,6 @@ class Navigation extends HTMLElement {
             <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
           </ul>
         </div>
-
-    <!-- Contenu principal de la page -->
-    <div class="content">
-      
-<div>
-          <h2>Espace sécurisé</h2>
-          <p>
-            Vous êtes connecté. Vous pouvez maintenant accéder aux actions protégées.
-          </p>
-        </div>
-
-        <div>
-          <div>
-            <label for="scriptName">Nom du script</label>
-            <input
-              id="scriptName"
-              type="text"
-              placeholder="Ex: afficher.sh">
-          </div>
-
-          <div>
-            <button id="btnRun" type="button">
-              Exécuter
-            </button>
-            <button id="btnMe" type="button">
-              Mon profil
-            </button>
-            <button id="btnLogout" type="button">
-              Déconnexion
-            </button>
-          </div>
-
-          <p id="out"></p>
-        </div>
     `;
   }
 }
